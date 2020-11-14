de2b05c5f5ffb82360348dca8639468a26cb6d03
EmilioMendoza <Ironislife39@gmail.com>
2020-11-13 23:24:44 -0500

    More stuff

--- Main.py
+++ Main.py
@@ -2,9 +2,21 @@ from flask import *
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("Index.html")
+
+@app.route('/aboutus')
+def aboutUs():
+    return render_template("about.html")
+
+@app.route("/donate")
+def donate():
+    return render_template("Donate.html")
+
+@app.route("/gallery")
+def photoGallery():
+    return render_template("Gallery.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
